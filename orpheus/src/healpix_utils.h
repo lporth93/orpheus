e742e35821f2286d72cad788828ca8a06c9d1127
Lucas Porth <16361977+lporth93@users.noreply.github.com>
2026-09-17 13:01:05 +0200

    Updates to curved-sky aperture kernel: C source

--- orpheus/src/healpix_utils.h
+++ orpheus/src/healpix_utils.h
@@ -6,13 +6,21 @@
 extern "C" {
 #endif
 
+double hpx_nside2resol(long nside);
+long hpx_ang2pix_nest(long nside, const double *vec);
+void hpx_nest2ring_range(long nside, long p0, long n, long *out);
+void hpx_pix2vec_nest(long nside, long ipix, double *vec);
+void hpx_pix2vec_ring(long nside, long ipix, double *vec);
+
 long hpx_query_disc_nest(long nside, const double *vec, double radius,
                          long *out, long max_out);
 long hpx_query_disc_nest_ranges(long nside, const double *vec, double radius,
                                 long *out_lohi, long max_pairs);
-double hpx_nside2resol(long nside);
-long hpx_ang2pix_nest(long nside, const double *vec);
-void hpx_pix2vec_nest(long nside, long ipix, double *vec);
+long hpx_query_disc_nest_ranges_exact(long nside, const double *vec, double radius,
+                                      long *out_lohi, long max_pairs);
+long hpx_query_disc_ring_ranges_exact(long nside, const double *vec, double radius,
+                                      long *out_lohi, long max_pairs);
+
 
 #ifdef __cplusplus
 }
